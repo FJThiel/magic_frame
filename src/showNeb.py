a755b1f14b1abd039af7a3c208a4d96c5ab9700e
Felix J. Thiel <git@thiel.green>
2025-03-22 13:30:40 +0000

    naming changes

--- src/showNeb.py
+++ src/showNeb.py
@@ -7,24 +7,24 @@ from epaper import epd
 
 if os.path.isfile("./IMGP8320.JPG"):
     
-    self.processed_image = Image.open("./IMGP8320.JPG")
+    image = Image.open("./IMGP8320.JPG")
     print("got the image")
 
-    img_utils.resize_image(self.original_image, 800, 480)
+    processed_image = img_utils.resize_image(image, 800, 480)
 
     print("resized the image")
 
     # Since the colors of epaper displays are fairly bleak, 
     # we're brightening them up just a little bit.
     try:
-        converter = ImageEnhance.Color(self.processed_image)
-        self.processed_image = converter.enhance(1.3)
+        converter = ImageEnhance.Color(processed_image)
+        processed_image = converter.enhance(1.3)
         print("colour-corrected the image")
     except:
         print("Failed to color-correct the image!")
 
 
-    epd.display(self.processed_image)
+    epd.display(processed_image)
 else:
     print("Image not found")
     
